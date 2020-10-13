ad3ae8678e87b2284a44c86fefbc472f4931a88a
tcharmont <charmont.theophile@gmail.com>
2020-10-13 21:43:32 +0200

    Draw one universe and m mechanics

---
+++ boardgameidea.py
@@ -0,0 +1,25 @@
+import data
+
+import argparse
+import random
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", type=int, help="define the mechanics number")
+    parser.add_argument("-v", "--verbose", action="store_true", help="increase the verbosity")
+    args = parser.parse_args()
+    print("### Universe ###")
+    print(data.universes[random.randrange(len(data.universes))] + "\n")
+    mechanics = draw_mechanics(args.m)
+    print("### Mechanics ###")
+    for mechanic in mechanics:
+        print(mechanic + " : " + data.mechanics.get(mechanic))
+
+
+def draw_mechanics(nb):
+    return random.sample(list(data.mechanics.keys()), nb)
+
+
+if __name__ == "__main__":
+    main()
---
+++ data.py
@@ -0,0 +1,20 @@
+universes = [
+    "pirate",
+    "knight",
+    "post-apocalyptic"
+]
+
+mechanics = {
+    "Acting": "Games with the Acting mechanic require players to use some form of mime or mimicry to communicate with "
+              "the other players.",
+    "Action Drafting": "Players select from an assortment of Actions in a shared pool. The available Actions are "
+                       "limited in quantity, and once a player has chosen an Action it may not be chosen again. ",
+    "Action Points": "A player receives a number of Action Points on their turn. They may spend them on a variety of "
+                     "Actions.",
+    "Action Queue": "Players create Action Queues and perform them in sequence. Queues can either be \"Batch\" "
+                    "queues, where all actions are executed in sequence, or \"Rolling\" queues where actions are "
+                    "added to the end of the Queue, and the first action is then executed.",
+    "Action Retrieval": "Each player has a set of Actions available to them, embodied in cards, tokens, or some other "
+                        "affordance. Once performed, they are spent and may not be performed again until retrieved. "
+                        "Action Retrieval typically is itself an Action, or may take an entire turn."
+}
